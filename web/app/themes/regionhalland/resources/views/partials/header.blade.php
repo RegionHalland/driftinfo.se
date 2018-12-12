ba3c43031632791f9313aaed13256bd1b45f3a6f
Hydén Roland RGS IT APP <rhn014@lthalland.se>
2018-12-12 17:19:29 +0100

    test_001

---
+++ web/app/themes/regionhalland/resources/views/partials/header.blade.php
@@ -0,0 +1,25 @@
+    <header aria-label="sidhuvud">
+    <nav aria-label="toppmeny" class="container mx-auto pb1 pt0 pr5 small bredcrumbs z4" style="text-align: right; background-color: #F3F3F3; position: relative;">
+        <ul aria-label="Toppmenylänkar">
+            &nbsp;
+        </ul>
+    </nav>
+  
+    <nav class="py2 container z4" style="background-color: white; position: relative;">      
+        <div class="clearfix">
+            <div class="col col-4 pl4">
+                &nbsp;
+            </div>
+            <div class="col col-6">
+                <span>
+                    <a href="{!! env('WP_HOME') !!}" class="rh-site-nav__logo" aria-label="Till startsidan">
+                        <img src="{!! env('WP_HOME') !!}/include/img/navigation_logo.svg" alt="">
+                    </a>
+                </span>
+                <span class="ml1 pl1" style="border-left: 1px solid grey">
+                Driftstörningar
+                </span>
+            </div>
+        </div>
+    </nav>
+</header>
