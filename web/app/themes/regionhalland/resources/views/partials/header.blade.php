776067131056751063f64c757dc01abb4a74d38c
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-28 14:13:09 +0200

    Making space in header for positioning content

--- web/app/themes/regionhalland/resources/views/partials/header.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/header.blade.php
@@ -1,11 +1,11 @@
-<div class="rh-logo-sitename mx-auto" style="max-width: 1440px">
+<div class="pl1 rh-logo-sitename mx-auto" style="max-width: 1440px">
     <a href=".">
         <img class="rh-logo-sitename-logo" src="{!! env('WP_HOME') !!}/styleguide5.0.0/images/components/rh-logo-sitename/navigation_logo.svg" alt="Driftinformation från Region Halland - förstasida">
     </a>
 </div>
 
 <div style="background: #004890; color: white;">
-    <div class="mx-auto pt3 pb4" style="max-width: 1440px">
+    <div class="pl1 mx-auto pt3" style="padding-bottom: 5em; max-width: 1440px">
         <h1 class="h2" style="color:white;">
             Driftinformation
         </h1>
