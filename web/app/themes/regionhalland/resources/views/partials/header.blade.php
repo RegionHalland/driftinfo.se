a961ebe2349d4651dfa16782f9a4ea5539dae071
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-11 16:23:33 +0200

    Cleaning up the visual head#

--- web/app/themes/regionhalland/resources/views/partials/header.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<div class="pl1 rh-logo-sitename mx-auto" style="max-width: 1440px">
+<div class="pl1 py2 rh-logo-sitename mx-auto" style="max-width: 1440px">
     <a href=".">
         <img class="rh-logo-sitename-logo" src="{!! env('WP_HOME') !!}/styleguide5.0.0/images/components/rh-logo-sitename/navigation_logo.svg" alt="Driftinformation från Region Halland - förstasida">
     </a>
@@ -6,7 +6,7 @@
 
 <div style="background: #004890; color: white;">
     <div class="pl1 mx-auto pt3" style="padding-bottom: 5em; max-width: 1440px">
-        <h1 class="h2" style="color:white;">
+        <h1 class="h1" style="color:white;">
             Driftinformation
         </h1>
     </div>
