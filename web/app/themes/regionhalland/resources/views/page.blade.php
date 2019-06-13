13f5b2343b73590538c9febc6f08d5048fd53a65
rhn014 <Roland.Hyden@RegionHalland.se>
2019-06-13 12:06:29 +0200

    ny page-sida

--- web/app/themes/regionhalland/resources/views/page.blade.php
+++ web/app/themes/regionhalland/resources/views/page.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
     
-        <div class="rh-xpad-A pt1 pb3 clearfix center" style="max-width: 1440px;background: #FBFAF9">
+    <div class="rh-xpad-A pt1 pb3 clearfix center" style="max-width: 1440px">
         <div class="rh-xpad-B pt3 pb3" style="max-width: 1152px;background: white;">
             <main>
                 <div class="clearfix">
                     <div class="pl4 col col-12 md-col-1 hidden-sm">
                         &nbsp;
                     </div>
-                    <div class="col col-12 md-col-9 rh-article">
+                    <div class="col col-12 md-col-10 rh-article">
                         @while(have_posts()) @php(the_post())
                         <h1>{{ $post->post_title }}</h1><br><br>
                         {{ the_content() }}
