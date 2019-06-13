870e49e218cb32f33dbabc96d2ba4620374e3fac
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-13 15:13:55 +0200

    Cookie notice added - green though

--- web/app/themes/regionhalland/resources/views/layouts/app.blade.php
+++ web/app/themes/regionhalland/resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html>
+  @include('partials.cookie-notice')
   @include('partials.head')
   <body @php body_class() @endphp>
     @include('partials.header')
--- web/app/themes/regionhalland/resources/views/partials/header.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/header.blade.php
@@ -1,7 +1,6 @@
 <div class="pl1 py2 rh-logo-sitename mx-auto" style="max-width: 1152px">
     <a href="/"><img src="/include/img/rh-logo.jpg" alt="Länk till förstasidan"></a>
 </div>
-
 <div style="background: #004890; color: white;">
     <div class="pl1 mx-auto pt3" style="padding-bottom: 5em; max-width: 1152px">
         <a href="." style="text-decoration:none;"><h1 class="h1" style="display:inline; color:white;">
