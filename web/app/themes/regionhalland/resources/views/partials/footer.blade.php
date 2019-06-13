507628c8aa77cf64a84e00cdbd91ec1f057dfb65
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-13 15:10:07 +0200

    Footer links corrected

--- web/app/themes/regionhalland/resources/views/partials/footer.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/footer.blade.php
@@ -25,9 +25,9 @@
         <div class="left-align col col-12 sm-col-12 md-col-4 lg-col-4">
             <p class="h2 pt3">Om webbplatsen</p>
             <ul>
-                <li><a class="rh-link--navigation" href="">Om webbplatsen</a></li>
+                <li><a class="rh-link--navigation" href="/om-webbplatsen">Om webbplatsen</a></li>
                 <li><a class="rh-link--navigation" href="https://www.regionhalland.se/om-region-halland/dataskydd/">Behandling av personuppgifter</a></li>
-                <li><a class="rh-link--navigation" href="">Cookies</a></li>
+                <li><a class="rh-link--navigation" href="/om-webbplatsen">Cookies</a></li>
             </ul>
 
 
