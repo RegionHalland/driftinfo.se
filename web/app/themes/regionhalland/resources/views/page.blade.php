d41b072d588813083f73e25a2fce394ad99b80af
rhn014 <Roland.Hyden@RegionHalland.se>
2019-06-13 11:57:13 +0200

    Ny page-sida

--- web/app/themes/regionhalland/resources/views/page.blade.php
+++ web/app/themes/regionhalland/resources/views/page.blade.php
@@ -2,17 +2,26 @@
 
 @section('content')
     
-    <div class="mx-auto" style="max-width: 1440px;">
-        <div class="center pt3 pb4" style="max-width:1152px;">
-            <main id="main">
-                @while(have_posts()) @php(the_post())
-                    <h1>{{ the_title() }}</h1>
-                    <article class="rh-article pr6">
-                        {!! the_content() !!}
-                    </article>
-                @endwhile
+        <div class="rh-xpad-A pt3 pb3 clearfix center" style="max-width: 1440px;background: #FBFAF9">
+        <div class="rh-xpad-B pt3 pb3" style="max-width: 1152px;background: white;">
+            <main>
+                <div class="clearfix">
+                    <div class="pl4 col col-12 md-col-1 hidden-sm">
+                        &nbsp;
+                    </div>
+                    <div class="col col-12 md-col-9 rh-article">
+                        @while(have_posts()) @php(the_post())
+                        <h1>{{ $post->post_title }}</h1><br><br>
+                        {{ the_content() }}
+                        @endwhile
+                    </div>
+                    <div class="pl4 col col-12 md-col-2 hidden-sm">
+                        &nbsp;
+                    </div>
+                </div>
             </main>
         </div>
     </div>
+
 
 @endsection
