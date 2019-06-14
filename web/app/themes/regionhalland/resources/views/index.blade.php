5954a417d89914475dd5b0cdd8cc98fddd0f1d58
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-14 14:24:41 +0200

    Restoring old behaviour

--- web/app/themes/regionhalland/resources/views/index.blade.php
+++ web/app/themes/regionhalland/resources/views/index.blade.php
@@ -182,29 +182,27 @@
 
                 {{-- Rubrikrad, visa bara från tablet och uppåt --}}
 
-                    <div class="p1 hidden-sm">
-                        <div class="p2 mb2 rh-card">
-                            <div class="clearfix">
-                                <div class="col col-12 sm-col-4">
-                                    &nbsp;
-                                </div>
-                                <div class="col col-12 sm-col-2">
-                                    <strong>Område</strong>
-                                </div>
-                                <div class="col col-12 sm-col-2">
-                                    <strong>Start</strong>
-                                </div>
-                                <div class="col col-12 sm-col-2">
-                                    <strong>Beräknat avslut</strong>
-                                </div>
-                                <div class="col col-12 sm-col-2">
-                                    <strong>Uppdateringar</strong>
-                                </div>
-                                <div class="col col-12 sm-col-2">
-                                </div>
+                    <header class="clearfix mt3 p1 hidden-sm">
+                        <div class="p2">
+                            <div class="col col-12 sm-col-4">
+                                &nbsp;
+                            </div>
+                            <div class="col col-12 sm-col-2">
+                                <strong>Område</strong>
+                            </div>
+                            <div class="col col-12 sm-col-2">
+                                <strong>Start</strong>
+                            </div>
+                            <div class="col col-12 sm-col-2">
+                                <strong>Beräknat avslut</strong>
+                            </div>
+                            <div class="col col-12 sm-col-2">
+                                <strong>Uppdateringar</strong>
+                            </div>
+                            <div class="col col-12 sm-col-2">
                             </div>
                         </div>
-                    </div>
+                    </header>
 
 
                     <div class="p1">
