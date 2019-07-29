d897501ec574e2d76b7a4806c414832d696479de
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-29 17:12:36 +0200

    Saknat lang-attribut på html-taggen

--- web/app/themes/regionhalland/resources/views/layouts/app.blade.php
+++ web/app/themes/regionhalland/resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html data-server="{!! env('SITE_SERVER') !!}" data-version="1.0.0">
+<html lang="sv-se" data-server="{!! env('SITE_SERVER') !!}" data-version="1.0.0">
   @include('partials.sitewide.jump-to-content')
   @include('partials.messages.cookie-notice')
   @include('partials.sitewide.html-head')
