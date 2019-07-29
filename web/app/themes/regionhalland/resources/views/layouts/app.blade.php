e53cef8f10dafe88a945a6817049537a43f5c3b5
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-29 17:20:21 +0200

    Saknat lang attribut på html-taggen

--- web/app/themes/regionhalland/resources/views/layouts/app.blade.php
+++ web/app/themes/regionhalland/resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html data-server="{!! env('SITE_SERVER') !!}" data-version="1.0.0">
+<html lang="sv-se" data-server="{!! env('SITE_SERVER') !!}" data-version="1.0.0">
   @include('partials.sitewide.html-head')
   <body @php body_class() @endphp>
     <header>
