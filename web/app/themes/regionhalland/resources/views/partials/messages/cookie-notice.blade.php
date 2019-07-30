ce3d6997ea8ca18c733a1921d03c47493553e92a
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-30 14:01:58 +0200

    Cookie notisen ingår nu i styleguide 1.3.0 som används

--- web/app/themes/regionhalland/resources/views/partials/messages/cookie-notice.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/messages/cookie-notice.blade.php
@@ -1,11 +1,13 @@
 @php($checkCookieNotice = check_region_halland_cookie_notice())
 @if ($checkCookieNotice == false)
 	@php($cookie_notice = get_region_halland_cookie_notice())
-	<div class="rh-cookie" style="background-color: rgba(108,162,213,0.3);">
-		<span class="rh-cookie-icon"></span>
-		<p class="rh-cookie-description">
-			{!! $cookie_notice['message'] !!}
-		</p>
-		<button id="cookie-consent" style="font-size:1em; background-color: #004890;width:13ex;" class="rh-button rh-button--primary cookie-button" aria-label="Acceptera cookies och dölj meddelandet" role="button">{!! $cookie_notice['button'] !!}</button>
+	<div style="background-color: rgba(108,162,213,0.3);">
+		<div class="rh-cookie mx-auto" style="max-width:1440px;">
+			<span class="rh-cookie-icon"></span>
+			<p class="rh-cookie-description">
+				{!! $cookie_notice['message'] !!}
+			</p>
+			<button id="cookie-consent" style="font-size:1em; background-color: #004890;width:13ex;" class="rh-button rh-button--primary cookie-button" aria-label="Acceptera cookies och dölj meddelandet" role="button">{!! $cookie_notice['button'] !!}</button>
+		</div>
 	</div>
 @endif
