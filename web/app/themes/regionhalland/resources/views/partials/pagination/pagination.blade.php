6ce6e899bf00aaab407fe6371fe1372b80d64b73
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-11-11 21:45:36 +0200

    Korrekt paginering

--- web/app/themes/regionhalland/resources/views/partials/pagination/pagination.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/pagination/pagination.blade.php
@@ -1,4 +1,4 @@
-@if($myPagination['total_pages'] >= 2)
+@if($myPagination['total_pages'] >= 1)
     <div class="pb2" style="display:flex; justify-content: center;">
         @if($myPagination['antal_items'] == 1)
             <h2 class="h3" style="border-bottom: 4px solid #004890">{{ $myPagination['antal_items'] }} driftstörning - sida {{ $myPagination['current_page'] }} av {{ $myPagination['total_pages'] }}</h2>
@@ -6,17 +6,20 @@
             <h2 class="h3" style="border-bottom: 4px solid #004890">{{ $myPagination['antal_items'] }} driftstörningar - sida {{ $myPagination['current_page'] }} av {{ $myPagination['total_pages'] }}</h2>
         @endif
     </div>
-    <div class="pb3" style="display:flex; justify-content: center;">
-        <span><a class="rh-pagination-link rh-pagination-link-previous" style="line-height: 3;" href="./?sida={{ $myPagination['first_page'] }}&oid={{ $oid }}&sid={{ $sid }}"><<</a></span>
-        <span><a class="rh-pagination-link" style="line-height: 3;" href="./?sida={{ $myPagination['prev_page'] }}&oid={{ $oid }}&sid={{ $sid }}"><</a></span>
+    <div class="rh-pagination pb3" style="display:flex; justify-content: center;">
+        <span><a class="rh-pagination-link rh-pagination-group rh-pagination-link-previous" href="./?sida={{ $myPagination['first_page'] }}&oid={{ $oid }}&sid={{ $sid }}"><<</a></span>
+        <span><a class="rh-pagination-link rh-pagination-group" href="./?sida={{ $myPagination['prev_page'] }}&oid={{ $oid }}&sid={{ $sid }}"><</a></span>
         @foreach ($myPagination['start_end'] as $start_end)
             @if($myPagination['current_page'] == $start_end['number'])
-                <strong><a class="rh-pagination-link" style="line-height: 3;background-color: #004890;color:#ffffff;" href="./?sida={{ $start_end['number'] }}&oid={{ $oid }}&sid={{ $sid }}">{!! $start_end['number'] !!}</a></strong>
+                <strong><a class="rh-pagination-link rh-pagination-group rh-pagination-link--current" href="./?sida={{ $start_end['number'] }}&oid={{ $oid }}&sid={{ $sid }}">{!! $start_end['number'] !!}</a></strong>
             @else
-                <span><a class="rh-pagination-link"  style="line-height: 3;" href="./?sida={{ $start_end['number'] }}&oid={{ $oid }}&sid={{ $sid }}">{!! $start_end['number'] !!}</a></span>
+                <span><a class="rh-pagination-link rh-pagination-group" href="./?sida={{ $start_end['number'] }}&oid={{ $oid }}&sid={{ $sid }}">{!! $start_end['number'] !!}</a></span>
             @endif
         @endforeach
-        <span><a class="rh-pagination-link"  style="line-height: 3;" href="./?sida={{ $myPagination['next_page'] }}&oid={{ $oid }}&sid={{ $sid }}">></a></span>
-        <span><a class="rh-pagination-link rh-pagination-link-next"  style="line-height: 3;" href="./?sida={{ $myPagination['last_page'] }}&oid={{ $oid }}&sid={{ $sid }}">>></a></span>
+        <span><a class="rh-pagination-link rh-pagination-group" href="./?sida={{ $myPagination['next_page'] }}&oid={{ $oid }}&sid={{ $sid }}">></a></span>
+        <span><a class="rh-pagination-link rh-pagination-group rh-pagination-link-next" href="./?sida={{ $myPagination['last_page'] }}&oid={{ $oid }}&sid={{ $sid }}">>></a></span>
     </div>
 @endif
+
+
+
