3f86e3b79a6dc035fc48bfbb3e1efbc7bc19b83e
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-11-14 17:38:24 +0200

    Matomo trackingkod inlagd

--- web/app/themes/regionhalland/resources/views/partials/sitewide/html-head.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/sitewide/html-head.blade.php
@@ -22,4 +22,19 @@
 	<script src="{!! env('WP_HOME') !!}/include/scripts/jquery.3.3.1.min.js?ver=3.3.1"></script>
 	<script src="{!! env('WP_HOME') !!}/styleguide5.0.0/js/components.js"></script>
 	<script src="{!! env('WP_HOME') !!}/include/scripts/application.js"></script>
+	<!-- Matomo -->
+	<script type="text/javascript">
+		var _paq = window._paq || [];
+		/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
+		(function() {
+			var u="//matomo.regionhalland.se/";
+			_paq.push(['setTrackerUrl', u+'matomo.php']);
+			_paq.push(['setSiteId', '2']);
+			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+			g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+		})();
+	</script>
+	<!-- End Matomo Code -->
 </head>
