0a14e69ac516a0a632cfa98c0da1fb7ccf53a9f0
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-11-14 17:36:46 +0200

    Test av ny headerkod

--- web/app/themes/regionhalland/resources/views/layouts/app.blade.php
+++ web/app/themes/regionhalland/resources/views/layouts/app.blade.php
@@ -1,6 +1,14 @@
-@php(header("Cache-Control: no-cache, no-store, must-revalidate"))
-@php(header("Pragma: no-cache"))
-@php(header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"))
+<?php
+  $headers = array(
+    'Cache-Control' => 'no-cache, no-store, must-revalidate',
+    'Pragma' => 'no-cache',
+    'Expires' => 'Mon, 26 Jul 1997 05:00:00 GMT',
+  );
+
+  foreach ($headers as $headerType => $headerValue) {
+      header($headerType . ': ' . $headerValue);
+  }
+?>
 <!doctype html>
 <html lang="sv-se" data-server="{!! env('SITE_SERVER') !!}" data-version="1.5.0" style="height: 101%">
   @include('partials.sitewide.html-head')
